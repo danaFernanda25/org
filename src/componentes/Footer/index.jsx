c12d1ca242a0cb156f56dee983262205c1e881f0
dana <fernanda.cordoba2504@gmail.com>
2023-09-23 21:08:34 -0500

    cambio en el footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,18 +3,18 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.facebook.com/' target="blank">
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.twiter.com/' target="blank">
                 <img src="/img/twitter.png" alt='twitter' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/' target="blank">
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
         <img src='/img/Logo.png' alt='org' />
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado por Dana Cordoba</strong>
     </footer>
 }
 
