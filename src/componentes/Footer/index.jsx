b5a78551804ed5c7ce609904c5feef0d79923e10
dana <fernanda.cordoba2504@gmail.com>
2023-09-27 20:58:28 -0500

    terminado

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,7 +1,7 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
+    return <footer className='footer' style={{ backgroundImage: "url(/img/Footer.png)" }}>
         <div className='redes'>
             <a href='https://www.facebook.com/' target="blank">
                 <img src="/img/facebook.png" alt='Facebook' />
